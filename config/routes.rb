328916af1eb8753fbfc408e4b9bb10e4676c7acd
Abdullah Numan <anewman15@hotmail.com>
2021-03-17 18:54:21 +0600

    Set root route to user/sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :users, except: [:edit, :update, :destroy] do
     resources :books, except: :show
   end
+
+  devise_scope :user do
+    root 'users/sessions#new'
+  end
 end
