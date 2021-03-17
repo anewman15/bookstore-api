8b7b33f902e37213ff4cd61129f3031a9d4e2120
Abdullah Numan <anewman15@hotmail.com>
2021-03-17 14:40:34 +0600

    Add username field to User and run db:migrate

--- db/migrate/20210317083210_devise_create_users.rb
+++ db/migrate/20210317083210_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :username,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -36,6 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
+    add_index :users, :username,                unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
