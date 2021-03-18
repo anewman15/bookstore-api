2325f811112465dd66f01475ff2685c6a4219eb1
Abdullah Numan <anewman15@hotmail.com>
2021-03-18 17:15:54 +0600

    Add @categories to new action, redirect_to to create action in books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,23 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :delete]
+
   def new
     @book = Book.new
+    @categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi']
   end
 
   def create
     @book = current_user.books.build(book_params)
     if @book.save
-      render json: { message: Book created successfully }, notice: 'Book created successfully'
+      redirect_to action: 'show', id: @book.id, notice: 'Book created successfully'
     else
       render 'new'
     end
   end
-
+  
   private
 
   def book_params
-    params.require(:book).permit(:title, :category)
+    params.require(:book).permit(:title, :category, :user_id)
   end
 end
