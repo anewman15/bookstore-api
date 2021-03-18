ab876e8a323bf5a6380166d6518a0b06a7096ae0
Abdullah Numan <anewman15@hotmail.com>
2021-03-18 17:27:58 +0600

    Generate views for books_controller

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
 
+  describe "GET /new" do
+    it "returns http success" do
+      get "/books/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/books/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/books/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/books/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
