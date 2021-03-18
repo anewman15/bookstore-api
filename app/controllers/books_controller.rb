43f48e7f44eaba1611152a3b9a6fbe58ebbdd2c7
Abdullah Numan <anewman15@hotmail.com>
2021-03-18 13:36:59 +0600

    Sanitize book params and define create action in books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,19 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      render json: { message: Book created successfully }, notice: 'Book created successfully'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :category)
+  end
 end
