11b131b75272c9503c7c45e87e8f3d545be14f2b
Abdullah Numan <anewman15@hotmail.com>
2021-03-18 17:29:53 +0600

    Include :show in :books resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/edit'
+  get 'books/index'
+  get 'books/show'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
   resources :users, except: [:edit, :update, :destroy] do
-    resources :books, except: :show
+    resources :books
   end
 
   devise_scope :user do
