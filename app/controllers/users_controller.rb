308d82e7d4093ce26c5985ddfcb5c52361330efc
Abdullah Numan <anewman15@hotmail.com>
2021-03-20 21:31:39 +0600

    Define create method for users with json output

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @user = User.new(user_create_params)
+
+    if @user.save
+      render json: @user, status: :ok
+    elsif User.exists?(username: @user.username) || User.exists?(email: @user.email)
+      render json: { message: 'User exists already' }
+    elsif @user.password != @user.password_confirmation
+      render json: { message: 'Passwords mismatch' }
+    else
+      render json: { message: 'User not created' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_create_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
