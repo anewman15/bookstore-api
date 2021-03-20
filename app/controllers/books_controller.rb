c2ce2f1f47358ff2c46cfe6a6be9d4728665a643
Abdullah Numan <anewman15@hotmail.com>
2021-03-20 23:10:38 +0600

    Modify create action in books_controller to render json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,23 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :delete]
-
-  def new
-    @book = Book.new
-    @categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi']
-  end
+  skip_before_action :verify_authenticity_token
 
   def create
-    @book = current_user.books.build(book_params)
-    if @book.save
-      redirect_to action: 'show', id: @book.id, notice: 'Book created successfully'
+    user = User.find_by(username: book_params[:username])
+    if user
+      @book = Book.new(user_id: user.id, title: book_params[:title], category: book_params[:category])
+      if @book.save
+        render json: @book, status: :ok
+      else
+        render json: { message: 'Book not created' }
+      end
     else
-      render 'new'
+      render json: { message: 'User does not exist. Please sign up' }
     end
-  end
-
-  def show
-    @book = Book.find(params[:id])
-    render json: @book
-  end
-
-  def index
-    render json: Book.all
   end
 
   private
 
-  def book_params
-    params.require(:book).permit(:title, :category, :user_id)
+  def create_book_params
+    params.require(:book).permit(:username, :title, :category)
   end
 end
