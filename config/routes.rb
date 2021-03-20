30f186fee5b72d4fc127b047fb01b39f2adf7abe
Abdullah Numan <anewman15@hotmail.com>
2021-03-20 23:12:50 +0600

    Remove nested routing for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :books, only: :index
+  resources :books, only: [:index, :create, :update, :destroy]
 
   namespace :user do
     resources :sessions, only: [:new, :create]
   end
 
-  resources :users, except: [:edit, :update, :destroy] do
-    resources :books
-  end
+  resources :users, except: [:edit, :update, :destroy]
 end
