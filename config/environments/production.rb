d121d1e0643722d5738b2e6d6fbfda2ee80acb02
Abdullah Numan <anewman15@hotmail.com>
2021-03-22 15:13:36 +0600

    Update host to config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,5 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.hosts << ".anewman15-book-tracker-api.herokuapp.com"
+  config.hosts << "anewman15-book-tracker-api.herokuapp.com"
 end
